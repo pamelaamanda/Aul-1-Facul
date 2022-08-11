a3a9550fd47b2e1c110046f7f9a00b537ee2bf8d
Pamela Amanda <72305176+pamelaamanda@users.noreply.github.com>
2022-08-10 23:41:33 -0300

    Add files via upload

--- main2.cpp
+++ main2.cpp
@@ -4,29 +4,23 @@
 
 int main(int argc, char** argv) {
 	
+	float salario, inss, ir, sal_liquido;
+	printf("Digite o seu salario bruto");
 	
-	float nota1;
-	float nota2;
-	float media;
+	scanf("%f", &salario);
 	
-	printf ("insira nota 1: \n");
-	scanf ("%f", &nota1);
+	if (salario<=1693.72)
+	{
+		inss = salario * 0.08;
+	}
+	else
+	if(salario>=1693.73&& salario<= 2822.90)
+	{
+		inss = salario * 0.09;
+	}
+	sal_liquido = (salario- inss)-ir;
+    printf( "\nDesconto do INSS e: %.2f\n\n",inss);
 	
-	
-	
-	
-	printf ("insira nota 2:");
-	scanf ("%f", &nota2);
-	
-	
-	media = (nota1 + nota2)/2;
-	printf("media = %f", media);
-	
-	
-	
-	
-	
-	
-	
+		printf ("\n Salario liquido %2.f\n", sal_liquido);
 	return 0;
 }
